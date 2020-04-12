7f30a9732692a6aa4e97630fe99db2ceafb91486
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2020-04-11 19:26:25 -0500

    Set Up

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,43 @@
+// Use D3 fetch to read the JSON file
+// The data from the JSON file is arbitrarily named importedData as the argument
+d3.json("samples.json").then((importedData) => {
+    console.log(importedData);
+    var data = importedData;
+
+    // Sort the data array using the greekSearchResults value
+    data.sort((a, b) => parseFloat(b.samples[2]["sample_values"]) - parseFloat(a.samples[2]["sample_values"]);
+    });
+
+    // Slice the first 10 objects for plotting
+    data = data.slice(0, 10);
+
+    // Reverse the array due to Plotly's defaults
+    data = data.reverse();
+
+    // Trace1 for the Greek Data
+    var trace1 = {
+        x: data.map(row => row.sample_values),
+        y: data.map(row => row.otu_ids),
+        text: data.map(row => row.otu_labels),
+        name: "OTUs",
+        type: "bar",
+        orientation: "h"
+    };
+
+    // data
+    var chartData = [trace1];
+
+    // Apply the group bar mode to the layout
+    var layout = {
+        title: "Bacteria Top 10 OTUs search results",
+        margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100
+        }
+    };
+
+    // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("plot", chartData, layout);
+});
